19c916b6b2bf320ee8cf53be737592386ff67648
PhatNgo03 <phatngo1908@gmail.com>
2025-05-03 13:23:45 +0700

    install pug

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("OK");
+import dotenv from 'dotenv';
+dotenv.config();
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+const port = process.env.PORT;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.render("client/pages/topics/index.pug");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
