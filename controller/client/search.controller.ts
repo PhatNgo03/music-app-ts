6219e30c48b8c21c073b889acb75b664af61638f
PhatNgo03 <phatngo1908@gmail.com>
2025-05-06 16:27:32 +0700

    search function with suggestions

--- controller/client/search.controller.ts
+++ controller/client/search.controller.ts
@@ -3,9 +3,11 @@ import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
 import { convertToSlug } from "../../helpers/convertToSlug";
 
-// [GET] /search/result
+// [GET] /search/:type
 export const result = async (req: Request, res: Response) => {
   try {
+    const type= req.params.type;
+
     const keyword: string  = `${req.query.keyword}`;
 
     let newSongs: any[] = [];
@@ -30,16 +32,42 @@ export const result = async (req: Request, res: Response) => {
           _id: item.singerId
         });
 
-        (item as any).infoSinger = infoSinger;
+        // (item as any).infoSinger = infoSinger;
+        newSongs.push({
+          id: item.id,
+          title: item.title,
+          avatar: item.avatar,
+          like: item.like,
+          slug: item.slug,
+          infoSinger: {
+            fullName: infoSinger?.fullName
+          }
+        })
       }
-
-      newSongs = songs;
+      // newSongs = songs;
     }
-    res.render("client/pages/search/result", {
-      pageTitle: `Kết quả : ${keyword}`,
-      keyword: keyword,
-      songs: newSongs
-    });
+    switch (type){
+      case "result":
+        res.render("client/pages/search/result", {
+          pageTitle: `Kết quả : ${keyword}`,
+          keyword: keyword,
+          songs: newSongs
+        });
+        break;
+      case "suggest":
+        res.json({
+          code:200,
+          message: "Thành công!",
+          songs: newSongs
+        })
+        break;
+      default:
+        res.json({
+          code:400,
+          message: "Thất bại!",
+        });
+        break;
+    }
   } catch (error) {
     res.status(500).send("Server error");
   }
